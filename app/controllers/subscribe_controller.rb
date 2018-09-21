989ce61c32e4884d632f448450818bf3a6cea123
Sal Tardibuono <s.tardibuono@live.com>
2018-09-21 08:10:36 -0700

    added eval to sendgrid response

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,6 +1,6 @@
 class SubscribeController < ApplicationController
 
-  require 'uri'
+  # require 'uri'
   require 'net/http'
 
   def new
@@ -13,6 +13,7 @@ class SubscribeController < ApplicationController
     url = URI("https://api.sendgrid.com/v3/contactdb/recipients")
 
     http = Net::HTTP.new(url.host, url.port)
+
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
@@ -23,10 +24,47 @@ class SubscribeController < ApplicationController
 
     response = http.request(request)
 
-    final_response = response.read_body # use final response for error handling -- if error_count > 0 ....
+    evaluated_response = eval(response.body)
+
+    if evaluated_response[:error_count] == 0
+      redirect_back fallback_location: root_url, notice: 'You have subscribed! Yay!'
+    else
+      error_message = evaluated_response[:errors][0][:message]
+      redirect_back fallback_location: root_url, alert: "Your subscription failed. #{error_message}"
+    end
+
+
+    # byebug
+
+    #final_response = response.read_body # use final response for error handling -- if error_count > 0 ....
+
+    ### try retrieve_transactional_template
+
+    # template_url = "d-8cfd1172c1bf4a6ea3369044cade8606"
+    # url = URI("https://api.sendgrid.com/v3/templates/#{template_url}")
+    #
+    # http = Net::HTTP.new(url.host, url.port)
+    # http.use_ssl = true
+    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    #
+    # request = Net::HTTP::Get.new(url)
+    # request["authorization"] = "Bearer #{Rails.application.credentials.dig(:send_grid, :api_key)}"
+    # request.body = "{}"
+    #
+    # response = http.request(request)
+    # puts response.read_body
 
     # if final_response
-    redirect_to root_url, notice: 'You have subscribed! Yay!'
+
+  end
+
+
+  private
+
+  def retrieve_transactional_template
+    id = 'd-8cfd1172c1bf4a6ea3369044cade8606'
+
+
   end
 
 end
