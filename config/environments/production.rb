bb2424b86dbb55d4d70e6cfae2b0506c7ff6bc2b
Sal Tardibuono <s.tardibuono@live.com>
2018-09-30 10:51:23 -0700

    update environment for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
 
+
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { :host => 'murmuring-savannah-11483.herokuapp.com' }
+
+
+
+
 
   config.public_file_server.enabled = true
 
