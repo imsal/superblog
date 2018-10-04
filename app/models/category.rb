480b6615b5532370823cc12a6cc5f254321c0b11
Sal Tardibuono <s.tardibuono@live.com>
2018-10-03 19:13:57 -0700

    select category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   has_many :posts
 
   scope :main_categories, -> { where(parent_id: nil) }
+
   has_many :sub_categories, class_name: 'Category', foreign_key: 'parent_id'
 
   before_save :set_slug
@@ -9,6 +10,14 @@ class Category < ApplicationRecord
   before_update :set_slug
 
   has_one_attached :image
+
+  def Category.all_sub_categories
+    Category.all - Category.main_categories
+  end
+
+  def category_parent
+    return Category.find(self.parent_id)
+  end
 
   def to_param
     slug
