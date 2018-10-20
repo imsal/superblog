b98368201aa0b68fb29a8dee1330363b3261e121
Sal Tardibuono <s.tardibuono@live.com>
2018-10-20 12:59:36 -0700

    updated credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'murmuring-savannah-11483.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'k3urtb3fxydecldddxm4sq.herokuapp.com' }
 
 
 
